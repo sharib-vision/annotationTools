b0df42f673e1686d4ab6a888d52c9917b4b45e6f
sharib ali <sharib.ali@eng.ox.ac.uk>
2018-11-21 15:57:32 +0000

    allows tif mask channels

--- evaluation/dice_IoU_evaluationMetrics.py
+++ evaluation/dice_IoU_evaluationMetrics.py
@@ -7,6 +7,14 @@ Created on Mon Nov 19 22:57:04 2018
 """
 import numpy as np
 
+def file_lines_to_list(path):
+  # open txt file lines to a list
+  with open(path) as f:
+    content = f.readlines()
+  # remove whitespace characters like `\n` at the end of each line
+  content = [x.strip() for x in content]
+  return content
+
 def calculate_confusion_matrix_from_arrays(prediction, ground_truth, nr_labels):
     replace_indices = np.vstack((ground_truth.flatten(),prediction.flatten())).T
     confusion_matrix, _ = np.histogramdd(replace_indices, bins=(nr_labels, nr_labels),range=[(0, nr_labels), (0, nr_labels)])
@@ -27,7 +35,6 @@ def calculate_iou(confusion_matrix):
             iou = float(true_positives) / denom
         ious.append(iou)
     return ious
-
 
 def calculate_dice(confusion_matrix):
     dices = []
@@ -69,6 +76,7 @@ if __name__ == '__main__':
     file_name_GT='../masks/Frame_00002233_mask_Artefact.png'
     file_name_predicted='../masks/Frame_00002233_mask_artefact_predicted.png'
     
+    
     y_true = (cv2.imread(str(file_name_GT), 0) > 0).astype(np.uint8)
  
 #    pred_image = (cv2.imread(str(pred_file_name), 0) > 255 * 0.5).astype(np.uint8)
@@ -81,4 +89,48 @@ if __name__ == '__main__':
     print(result_jaccard)
     
 #   result_dice += [dice(y_true, y_pred)]
+    
+    testing=1
+    if testing:
+        frameName = 'Frame_00002233'
+        import tifffile as tiff
+#        GT masks
+        file_name_GT_txt ='../masks/'+frameName+'_GT_mask.txt'
+        file_name_GT_maskImage ='../masks/'+frameName+'_GT_mask.tiff'
+        
+        y_true_Array = tiff.imread(file_name_GT_maskImage)
+        
+#        =============>>>>>>>>>>>>>>>>>predicted maskes (change me!!!)
+#        file_name_eval_txt ='../masks/'+frameName+'_eval_mask.txt'
+#        file_name_eval_maskImage ='../masks/'+frameName+'_eval_mask.tiff'
+        file_name_eval_txt=file_name_GT_txt
+        file_name_eval_maskImage=file_name_GT_maskImage
+        
+#        read txt files as array
+        textfile_GT = file_lines_to_list(file_name_GT_txt)
+        textfile_eval = file_lines_to_list(file_name_eval_txt)
+        
+        y_pred_Array = tiff.imread(file_name_eval_maskImage)
+        
+        dice_val =[]
+        jaccard_val=[]
+        for i in range(len(textfile_GT)):
+            for j in range (len(textfile_eval)):
+                compare_class = [textfile_GT[i] == textfile_eval[j]]
+                if str(compare_class[0])=='True':
+#                    print([textfile_GT[i] == textfile_eval[j]])
+                    y_true = (((y_true_Array[i, :, :])> 0).astype(np.uint8))
+                    y_pred = (((y_true_Array[j, :, :])> 0).astype(np.uint8))
+                    result_dice = [dice(y_true.flatten(), y_pred.flatten())]
+                    result_jaccard = [jaccard(y_true.flatten(), y_pred.flatten())]
+                    
+                    dice_val.append(result_dice)
+                    jaccard_val.append(result_jaccard)
+
+                    
+        print('diceval {}'.format(dice_val))
+#    get mean values
+        meanDiceVal = np.mean(dice_val)  
+        meanJaccard = np.mean(jaccard_val)  
+        print('mean dice {} and mean jaccard {}'.format(meanDiceVal, meanJaccard))
     
